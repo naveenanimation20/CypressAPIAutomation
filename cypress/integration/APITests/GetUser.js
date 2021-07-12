a953aa7612fb73b17163e422285c5201a5b30ab0
Naveen <naveenanimation20@gmail.com>
2021-07-12 12:18:56 +0530

    added GET and POST call

--- cypress/integration/APITests/GetUser.js
+++ cypress/integration/APITests/GetUser.js
@@ -1,20 +1,15 @@
 /// <reference types="Cypress" />
-
 const dataJson = require('../../fixtures/example')
-
-
 describe('api test cases',() => {
     var payload = ""
     let accesstoken= 'f6d5f728817405850f2f6aff23f8693a6352ca375383a2529e7150a74a9a8de6'
-    it('get user', () => {
-        
+    it('get user', () => {        
         cy.request({
             method : 'GET',
             url : 'https://gorest.co.in/public-api/users/123/posts',
             headers: {
                 'authorization': 'Bearer '+accesstoken,
               }
-              
         }).then((res)=>{
             expect(res.status).to.eq(200)
             expect(res.body.meta.pagination).has.property('limit',20)
@@ -33,7 +28,6 @@ describe('api test cases',() => {
         })
     })
 
-
     it('get user by id', () => {
         cy.request({
             method : 'GET',
@@ -41,17 +35,9 @@ describe('api test cases',() => {
             headers: {
                 'authorization': 'Bearer '+accesstoken,
               }
-              
         }).then((res)=>{
-
             expect(res.status).to.eq(200)
             expect(res.body.data.name).to.eq('Aatreya Khanna')
         })    
-
-    })
-
-
-   
-
-    
+    })  
 })
--- cypress/integration/APITests/PostUser.js
+++ cypress/integration/APITests/PostUser.js
@@ -1,22 +1,15 @@
 /// <reference types="Cypress" />
-
 const dataJson = require('../../fixtures/createuser')
-
 describe('post user request', () => {
 let accessToken = '007526d9efdbc07e084ff7a6d4cfcc90588fbe20641c00faebf45a7f3b2eaf33'
 let randomText = ""
 let testEmail = ""
-
     it('create user test', () => {
-
         var pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         for (var i = 0; i < 10; i++)
         randomText+=pattern.charAt(Math.floor(Math.random() * pattern.length));
         testEmail = randomText + '@gmail.com'
-
         cy.fixture('createuser').then((payload) =>{
-
-
         cy.request({
             method: 'POST',
             url: 'https://gorest.co.in/public/v1/users',
@@ -37,14 +30,7 @@ let testEmail = ""
             expect(res.body.data).has.property('name',payload.name)
             expect(res.body.data).has.property('status',payload.status)
             expect(res.body.data).has.property('gender',payload.gender)
-            
         })
-
-
-
-
     })
-
 })
-
 })
